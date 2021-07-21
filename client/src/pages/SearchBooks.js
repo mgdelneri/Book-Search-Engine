f11d73fc4f3276efa1071460b32368af507a7515
Marina <mgdelneri@gmail.com>
2021-07-21 16:46:50 -0500

    Fixed some bugs; Added dependencies to package.json files; Deleted API folder

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -15,6 +15,8 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
+
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
@@ -30,7 +32,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await fetch(`http://www.googleapis.com/books/v1/volumes?q=${searchinput}`);
+      const response = await fetch(`http://www.googleapis.com/books/v1/volumes?q=${searchInput}`);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
